fc03dd9f72ea200e1a0964bc4e837e3c93d610af
Jordan Jambazov <jordan.jambazov@gmail.com>
2014-07-17 00:23:39 +0300

    Fix restype

--- ctypes/persons.py
+++ ctypes/persons.py
@@ -9,5 +9,7 @@ class Person(ctypes.Structure):
 
 
 persons_handle = ctypes.cdll.LoadLibrary("/home/jordan/Sandbox/Persons/libperson.so")
-
+persons_handle.get_person.restype = Person
 person = persons_handle.get_person("Jordan", 21)
+
+print("The name of the person is {} and the age is {}".format(person.name, person.age))
